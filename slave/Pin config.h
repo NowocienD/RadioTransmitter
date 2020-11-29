5d36fa44fda020944ca51702a558725975ef7893
Dominik Nowocień <dominik.nowocien@gmail.com>
2020-11-29 14:16:13 +0100

    przeniesienie confiiguracji wyjsc procesora do osobnego pliku

---
+++ slave/Pin config.h
@@ -0,0 +1,34 @@
+/*
+ * Pin_config.h
+ *  Author: Dominik
+ */ 
+
+
+#ifndef PIN CONFIG_H_
+#define PIN CONFIG_H_
+
+#define led_PIN (1<<PB7)
+#define led_PORT PORTB
+
+#define led_off	led_PORT |= led_PIN
+#define led_change	led_PORT ^= led_PIN
+#define led_on led_PORT &= ~led_PIN
+
+#define BUTTON_PIN (1<<PD3)
+
+#define Switch1_PIN (1<<PD4)
+#define Switch2_PIN (1<<PD0)
+
+#define Switch1_enable_PullUP PORTD |= (Switch1_PIN)
+#define Switch1_disble_PullUP PORTD &= ~(Switch1_PIN)
+#define Switch2_enable_PullUP PORTD |= (Switch2_PIN)
+#define Switch2_disble_PullUP PORTD &= ~(Switch2_PIN)
+
+#define Switch1_HIGH (PIND & Switch1_PIN)
+#define Switch2_HIGH (PIND & Switch2_PIN)
+
+#define BUTTON_HIGH (PIND & BUTTON_PIN)
+
+
+
+#endif /* PIN CONFIG_H_ */
--- slave/main.c
+++ slave/main.c
@@ -6,33 +6,8 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
+#include "Pin config.h"
 
-
-#define led_PIN (1<<PB7)
-#define led_PORT PORTB
-
-#define led_off	led_PORT |= led_PIN
-#define led_change	led_PORT ^= led_PIN
-#define led_on led_PORT &= ~led_PIN
-
-#define BUTTON_PIN (1<<PD3)
-
-#define Switch1_PIN (1<<PD4)
-#define Switch2_PIN (1<<PD0)
-
-#define Switch1_enable_PullUP PORTD |= (Switch1_PIN)
-#define Switch1_disble_PullUP PORTD &= ~(Switch1_PIN)
-#define Switch2_enable_PullUP PORTD |= (Switch2_PIN)
-#define Switch2_disble_PullUP PORTD &= ~(Switch2_PIN)
-
-#define Switch1_HIGH (PIND & Switch1_PIN)
-#define Switch2_HIGH (PIND & Switch2_PIN)
-
-#define BUTTON_HIGH (PIND & BUTTON_PIN)
-
-
-
-#define  buffersize 16
 
 int main(void)
 {
