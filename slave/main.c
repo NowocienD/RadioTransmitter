4a8e9fadf5d6f2485a7ab49eb2361771f4ef44e3
Dominik Nowocień <dominik.nowocien@gmail.com>
2020-12-04 10:35:09 +0100

    mierzenie napięcia baterii w nieskonczonej petli

--- slave/main.c
+++ slave/main.c
@@ -7,6 +7,8 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include "Pin config.h"
+#include "Voltage.h"
+#include "UART.h"
 
 
 int main(void)
@@ -24,9 +26,15 @@ int main(void)
 	Switch1_disble_PullUP;
 	Switch2_disble_PullUP;	
 
+	VoltageMeasure_Init();
+	USART_Init(MYUBRR);
+
 	while (1)
 	{
-		
+		VoltageMeasure_Start();
+		_delay_ms(500);
+		USART_Transmit(VoltageMeasure_Get());
+		_delay_ms(500) ;
 	}
 }
 
