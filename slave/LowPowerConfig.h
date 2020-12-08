30b62563704cc14dbe6eb1990a8982ef760125d3
Dominik Nowocień <dominik.nowocien@gmail.com>
2020-12-08 20:31:20 +0100

    wydzielenie makr dla redukcji energii

--- slave/LowPowerConfig.h
+++ slave/LowPowerConfig.h
@@ -1,8 +1,15 @@
 #ifndef LOWPOWERCONFIG_H_
 #define LOWPOWERCONFIG_H_
 
-#define ADC_reducePower DIDR0 = 0x3F; // DIDR0 – Digital Input Disable Register 0 // DataSheet: this bit should be written logic one to reduce power consumption in the digital input buffer
-#define	ADC_off ADCSRA &=~(1<<ADEN);
-#define	AC_off ACSR |= (1<<ADC);
+#define AC_reducePower DIDR1 = 0x03
+#define ADC_reducePower DIDR0 = 0x3F // DIDR0 – Digital Input Disable Register 0 // DataSheet: this bit should be written logic one to reduce power consumption in the digital input buffer
+#define	ADC_off ADCSRA &=~(1<<ADEN)
+#define	AC_off ACSR |= (1<<ADC)
+
+#define Timer0_off TCCR0B &= ~(1<<CS02) & ~(1<<CS01) & ~(1<<CS00)
+#define Timer1_off TCCR1B &= ~(1<<CS12) & ~(1<<CS11) & ~(1<<CS10)
+#define Timer2_off TCCR2B &= ~(1<<CS22) & ~(1<<CS21) & ~(1<<CS20)
+
+#define PowerReducion_Enable PRR = ~(1<<PRSPI) & ~(1<<PRADC)
 
 #endif
--- slave/main.c
+++ slave/main.c
@@ -12,8 +12,8 @@
 
 uint8_t bufferA[16] = {0x44, 0x55, 0x50, 0x41,0xA5,0xA6,0xA7,0xA8,0xA9,0xB0,0xB1,0xB2,0xB3,0xB4,0xB5,0xB6};
 
-#define DoSleep2 do {		cli();	/*SMCR = (1<<SM1) | (1<<SE);*/ sleep_enable();		sleep_bod_disable();	sei();	sleep_cpu();		/*SMCR = 0x00;*/ sleep_disable();	} while (0);
-#define DoSleep do {		cli();	SMCR = (1<<SM1) | (1<<SE);		sleep_bod_disable();	sei();	sleep_cpu();		SMCR = 0x00;	} while (0);
+#define DoSleep2		do { cli();	/*SMCR = (1<<SM1) | (1<<SE);*/ sleep_enable();	sleep_bod_disable();sei(); sleep_cpu();	/*SMCR = 0x00;*/ sleep_disable();	} while (0);
+#define DoSleep	do { cli();	SMCR = (1<<SM1) | (1<<SE);						sleep_bod_disable();sei(); sleep_cpu();	  SMCR = 0x00;						} while (0);
 
 volatile uint8_t sleepPeriodCounter;
 volatile uint8_t sleepTime;
@@ -22,7 +22,13 @@ volatile uint8_t sleepTime;
 int main(void)
 {
 	MCUSR = 0x00; //MCU Status Register // zresetowanie flag resetu.
+	
 	ADC_reducePower;
+	AC_reducePower;
+	Timer0_off;
+	Timer1_off;
+	Timer2_off;
+	PowerReducion_Enable;
 	
 	//ustawenie wszystkich pinow jako wejscia
 	DDRB = 0x00;
@@ -58,11 +64,7 @@ int main(void)
 	while (1)
 	{
 		ADC_off;
-		AC_off;
-		TCCR0B &= ~(1<<CS02) & ~(1<<CS01) & ~(1<<CS00);
-		TCCR1B &= ~(1<<CS12) & ~(1<<CS11) & ~(1<<CS10);
-		TCCR2B &= ~(1<<CS22) & ~(1<<CS21) & ~(1<<CS20);
-		PRR = ~(1<<PRSPI) & ~(1<<PRADC) & (1<<PRUSART0);
+		AC_off; 
 		DoSleep;
 	}
 }
