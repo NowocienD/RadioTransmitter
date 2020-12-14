bc6c05e45f87cc8581b5e8485a1b01ea1a6185e4
Dominik Nowocień <dominik.nowocien@gmail.com>
2020-12-15 00:17:12 +0100

    dodnie losowosci

--- slave/Voltage.c
+++ slave/Voltage.c
@@ -13,24 +13,39 @@ void VoltageMeasure_Init()
 	ADCSRA =
 	(1<<ADPS2)|				// Prescaler Select Bits // min prescaler f.cpu/64 #TODO zmieniæ na /2 po zmniejszeniu taktowania uC
 	(1<<ADPS1)|
-	(0<<ADPS0)|	
+	(0<<ADPS0)|
 	(1<<ADIE);				// Interrupt Enable
 }
 
+void VoltageGivesRandomSeed(uint8_t randNumFromADC)
+{
+	seed <<= 1; ;
+	seed |= (randNumFromADC & 0x01);	
+}
+
+uint8_t VoltageGetSeed()
+{
+	return seed;
+}
+
 void VoltageMeasure_Start()
-{	
+{
 	ADCSRA |=
 	(1<<ADEN)|	// ADC Enable
 	(0<<ADSC);	// Start conversion //Not nessesary. After entering ADC noise reduction sleep mode ADS automaticaly starts coversion
 	
 	sei(); // #TODO prawdopodonnie do usuniêcia/ trzeba jakos rozwiazac prblem wylaczenie przerwan wewnatrz przerwan
-	DoAdcSleep;	
+	DoAdcSleep;
 }
 
 uint8_t VoltageMeasure_Get()
 {
+	// make sure ADCL is readed first
+	uint8_t ADC_LOW = ADCL;
 	// calculate voltage
-	float result = (1.1 * 1024)/((ADCH<<8) + ADCL);
+	float result = (1.1 * 1024)/((ADCH<<8) + ADC_LOW);
+	
+	VoltageGivesRandomSeed(ADC_LOW);
 	
 	// recaltulate result to batery percentage
 	uint8_t res = (result-MIN_BATTERY_VOLTAGE)/(MAX_BATTERY_VOLTAGE-MIN_BATTERY_VOLTAGE) * 100;
